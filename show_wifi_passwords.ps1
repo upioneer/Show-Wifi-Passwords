16bf2ac57dc9fadc979f72ffe5f1effc398b3a8f
upioneer <hgrand@outlook.com>
2023-05-09 09:38:35 -0500

    Add files via upload

---
+++ show_wifi_passwords.ps1
@@ -0,0 +1,9 @@
+(netsh wlan show profiles) `
+    | Select-String "\:(.+)$" `
+    | ForEach-Object{$ssid=$_.Matches.Groups[1].Value.Trim(); $_} `
+    | ForEach-Object{(netsh wlan show profile name="$ssid" key=clear)} `
+    | Select-String "Key Content\W+\:(.+)$" `
+    | ForEach-Object{$pass=$_.Matches.Groups[1].Value.Trim(); $_} `
+    | ForEach-Object{[PSCustomObject]@{ SSID=$ssid;Pass=$pass }} `
+    | Sort-Object SSID `
+    | Out-GridView
