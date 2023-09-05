d17e20c8dd7d31b2826e252387fd310c9ed42fca
upioneer <hgrand@outlook.com>
2023-09-05 18:53:07 -0500

    Add files via upload

---
+++ wifi_aggression.ps1
@@ -0,0 +1,35 @@
+﻿<#
+    Tx power: Highest
+    Preferred band: 5ghz
+    Roaming agressiveness: Highest
+
+    References
+        https://answers.microsoft.com/en-us/windows/forum/windows_10-networking/roaming-aggressiveness-doesnt-change-anything/52890427-78de-4aa6-9eba-0456041addf3
+#>
+
+function get-validvalues {
+    $CurrentFormatEnumerationLimit = $FormatEnumerationLimit
+    $FormatEnumerationLimit =-1
+
+    Get-NetAdapterAdvancedProperty W*i -DisplayName "Transmit Power" | ft -Wrap -AutoSize DisplayName, DisplayValue, ValidDisplayValues
+    Get-NetAdapterAdvancedProperty W*i -DisplayName "Preferred Band" | ft -Wrap -AutoSize DisplayName, DisplayValue, ValidDisplayValues
+    Get-NetAdapterAdvancedProperty W*i -DisplayName "Roaming Aggressiveness" | ft -Wrap -AutoSize DisplayName, DisplayValue, ValidDisplayValues
+
+    $FormatEnumerationLimit = $CurrentFormatEnumerationLimit
+}
+
+function get-currentvalues {
+    Get-NetAdapteradvancedProperty -Name “Wi-Fi” -DisplayName “Transmit Power”
+    Get-NetAdapteradvancedProperty -Name “Wi-Fi” -DisplayName “Preferred Band”
+    Get-NetAdapteradvancedProperty -Name “Wi-Fi” -DisplayName “Roaming Aggressiveness”
+}
+
+# Set Tx power
+Set-NetAdapteradvancedProperty -Name “Wi-Fi” -DisplayName “Transmit Power” –DisplayValue “5. Highest”
+
+# Set Preferred band
+Set-NetAdapteradvancedProperty -Name “Wi-Fi” -DisplayName “Preferred Band” –DisplayValue “3. Prefer 5GHz band”
+
+# Set Roaming Aggressiveness
+Set-NetAdapteradvancedProperty -Name “Wi-Fi” -DisplayName “Roaming Aggressiveness” –DisplayValue “1. Lowest”
+Set-NetAdapteradvancedProperty -Name “Wi-Fi” -DisplayName “Roaming Aggressiveness” –DisplayValue “5. Highest”
